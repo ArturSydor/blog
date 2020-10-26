75e1569d2e8cc655dc070f644c3c33f47b4ba760
artursydor <sydor.artur@ukr.net>
2020-10-27 00:29:23 +0200

    create register form

--- blog-client/src/app/app.module.ts
+++ blog-client/src/app/app.module.ts
@@ -4,15 +4,30 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { RegisterSuccessComponent } from './auth/register-success/register-success.component';
+import { LoginComponent } from './auth/login/login.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    RegisterComponent,
+    RegisterSuccessComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: 'register', component: RegisterComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ blog-client/src/app/auth/register/register.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {RegisterPayload} from "../register-payload";
+import {AuthService} from "../auth.service";
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  registerForm: FormGroup;
+  registerPayload: RegisterPayload;
+
+  constructor(private formBuilder: FormBuilder, private authService: AuthService) {
+    this.registerForm = this.formBuilder.group({
+      username:'',
+      email:'',
+      password:'',
+      confirmPassword:''
+    });
+    this.registerPayload = {
+      username:'',
+      email:'',
+      password:'',
+      confirmPassword:''
+    };
+  }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit() {
+    this.registerPayload.username = this.registerForm.get('username').value;
+    this.registerPayload.email = this.registerForm.get('email').value;
+    this.registerPayload.password = this.registerForm.get('password').value;
+    this.registerPayload.confirmPassword = this.registerForm.get('confirmPassword').value;
+
+    this.authService.register(this.registerPayload).subscribe(data => {
+      console.log('register success');
+    }, error => {
+      console.log('register fail');
+    });
+  }
+}
